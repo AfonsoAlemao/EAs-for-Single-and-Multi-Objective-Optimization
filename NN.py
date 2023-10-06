0030fa0d87a5bcbe030acb676d4d69f6b6338f1a
AfonsoAlemao <afonso.alemao01@gmail.com>
2023-10-06 13:59:16 +0100

    NN update

--- NN.py
+++ NN.py
@@ -49,7 +49,7 @@ df = df.drop(columns=['V_MemoryUsage','V_ProcessorLoad','V_InpNetThroughput','V_
 y = (np.array(df))[:,-1]
 X = (np.array(df))[:,:-1]
 
-X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.3, random_state=42)s
+X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.3, random_state=42)
 
 clf = MLP_training(X_train, y_train)
 
