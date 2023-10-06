bbd5b46d14ca7ab14c25202be6c43e68f606d574
Rui Daniel <ruipedrocd@gmail.com>
2023-10-06 12:35:06 +0100

    update codigo NN

--- NN.py
+++ NN.py
@@ -11,15 +11,14 @@ from sklearn.metrics import classification_report
 from sklearn.model_selection import GridSearchCV
 
 
-def MLP(X, y):
-    X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.3, random_state=42)
-
+def MLP_training(X_train, y_train):
     mlp_gs = MLPClassifier(max_iter=1000, verbose=False)
     parameter_space = {
         'classifier__activation': ['tanh', 'relu'],
         'classifier__solver': ['sgd', 'adam'],
-        'classifier__alpha': [0.0001, 0.05],
+        'classifier__alpha': [0.001, 0.05, 0.01],
         'classifier__learning_rate': ['constant', 'adaptive'],
+        'classifier__hidden_layer_sizes': [(8),(9,6),(8,7,6)]
     }
 
     # Create a pipeline with StandardScaler and MLPClassifier
@@ -32,21 +31,32 @@ def MLP(X, y):
     clf.fit(X_train, y_train) # X is train samples and y is the corresponding labels
 
     print('Best parameters found:\n', clf.best_params_)
-
-    # print('clf.get_params = ', clf.get_params, 'clf.best_params_ = ', clf.best_params_)
-    # print('clf.get_params = ', clf.get_params)
     
+    return clf
+
+def MLP_testing(clf): 
     y_true, y_pred = y_test , clf.predict(X_test)
 
     print('Results on the test set:')
     print(classification_report(y_true, y_pred))
+    
+    return y_pred
     
 
 import pandas as pd
 
 df = pd.read_csv('ACI23-24_Proj1_SampleData_new.csv', encoding='utf-8')
 
-y_iris = (np.array(df))[:,-1]
-X_iris = (np.array(df))[:,:-1]
+y = (np.array(df))[:,-1]
+X = (np.array(df))[:,:-1]
 
-MLP(X_iris, y_iris)
+X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.3, random_state=42)
+
+clf = MLP_training(X_train, y_train)
+
+ypred = MLP_testing(clf)
+
+df['CLPVariation_pred'] = ypred
+
+df.to_excel('MLP_Results.xlsx', index=False)
+df.to_csv('MLP_Results.csv', index=False, encoding='utf-8')
